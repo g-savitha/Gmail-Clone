5c65e6a721790c4c776bfff89630783565765172
G Savitha <mailto@gsavitha.in>
2021-04-23 15:06:17 +0530

    header section

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+          alt=""
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search Mail" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
